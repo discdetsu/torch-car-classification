1b00f5c449cd79e19d4aa5767238322888019509
discdetsu <disk.nitrome@gmail.com>
2022-11-08 15:19:36 +0700

    update gui2

--- gui.py
+++ gui.py
@@ -16,53 +16,58 @@ from tkinter import filedialog
 from video import *
 
 class App:
-     def __init__(self, window, window_title, video_source=0):
-         self.window = window
-         self.window.title(window_title)
-         self.window.geometry("1000x900")
-         self.video_source = video_source
- 
-         # open video source (by default this will try to open the computer webcam)
-         self.vid = MyVideoCapture(self.video_source)
- 
-         # Create a canvas that can fit the above video source size
-         self.canvas = tkinter.Canvas(window, width = self.vid.width, height = self.vid.height)
-         self.canvas.pack()
-         
-         # Create text label to display predicted classes and confident scores
-         self.labels = [Label(self.window, font= ("Ubuntu", 20), text='') for i in range(5)]
-         for label in self.labels:
-             label.pack(pady=10)
+    def __init__(self, window, window_title, video_source=0):
+        self.window = window
+        self.window.title(window_title)
+        self.window.geometry("1200x900")
+        self.video_source = video_source
 
-         # Button that lets the user take a snapshot
-         
-         self.delay = 15
-         self.update()
- 
-         self.window.mainloop()
- 
-     def snapshot(self):
-         # Get a frame from the video source
-         ret, frame = self.vid.get_frame()
-         if ret:
-             cv2.imwrite("frame-" + time.strftime("%d-%m-%Y-%H-%M-%S") + ".jpg", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
- 
-     def update(self):
-         # Get a frame from the video source
-         ret, frame = self.vid.get_frame()
-         if ret:
-             self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame),)
-             self.canvas.create_image(0, 0, image = self.photo, anchor = tkinter.NW)
-             pred_class, cf = self.predict(model, classes, frame)
-             
-             for index, pred in enumerate(zip(pred_class, cf)):
-                 
-                 text = '{} Conf: {:.4f}'.format(pred[0], pred[1])
-                 self.labels[index].config(text=text)
+        # open video source (by default this will try to open the computer webcam)
+        self.vid = MyVideoCapture(self.video_source)
+
+        # Create a canvas that can fit the above video source size
+        self.canvas = tkinter.Canvas(window, width = self.vid.width, height = self.vid.height)
+        self.canvas.pack(padx=5, pady=10)
+
+        self.label = Label(self.window, font= ("Ubuntu", 20), text='Model name')
+        self.label.pack(pady=10)
+
+        # Create text label to display predicted classes and confident scores
+        self.labels = [Label(self.window, font= ("Ubuntu", 20), text='') for i in range(5)]
+        for label in self.labels:
+            label.pack(padx=5, pady=10, side=tkinter.RIGHT)
+
+        # Button that lets the user take a snapshot
+        
+        self.delay = 15
+        self.update()
+
+        self.window.mainloop()
+
+    def snapshot(self):
+        # Get a frame from the video source
+        ret, frame = self.vid.get_frame()
+        if ret:
+            cv2.imwrite("frame-" + time.strftime("%d-%m-%Y-%H-%M-%S") + ".jpg", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+
+    def update(self):
+        # Get a frame from the video source
+        ret, frame = self.vid.get_frame()
+        if ret:
+            self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame),)
+            self.canvas.create_image(0, 0, image = self.photo, anchor = tkinter.NW)
+            pred_class, cf = self.predict(model, classes, frame)
+           
+            
+            for index, pred in enumerate(zip(pred_class, cf)):
                 
-         self.window.after(self.delay, self.update)
-     
-     def predict(self, model, classes, frame):
+                text = '{} Conf: {:.4f}'.format(pred[0], pred[1])
+                self.labels[index].config(text=text)
+
+
+        self.window.after(self.delay, self.update)
+    
+    def predict(self, model, classes, frame):
         
         pil_image = Image.fromarray(frame)
         transform = transforms.Compose([transforms.Resize((244,244)),
@@ -88,7 +93,6 @@ class App:
         cfscs = list(cfscs.detach().cpu().numpy().flat)
         
         return predicted, cfscs 
- 
  
 class MyVideoCapture:
      def __init__(self, video_source):
